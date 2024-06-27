9a9600c4544e030ee62da4dbe4958e546716240d
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-27 09:04:40 +0400

    remove old ui-button package

--- ui/button/lib/button.server.ts
+++
@@ -1,7 +0,0 @@
-import type * as T from "./Button.tsx"
-import * as C from "./Button.tsx"
-
-export type ButtonParameters = T.RootParameters
-export type ButtonVariant = T.RootVariant
-
-export const Button = Object.assign(C.Root, {})
--- ui/kit/lib/kit.server.ts
+++ ui/kit/lib/kit.server.ts
@@ -1,5 +1,4 @@
 export * from "@onlyoffice/documentation-ui-article-card"
-export * from "@onlyoffice/documentation-ui-button"
 export * from "@onlyoffice/documentation-ui-content"
 export * from "@onlyoffice/documentation-ui-font"
 export * from "@onlyoffice/documentation-ui-sr-only"
