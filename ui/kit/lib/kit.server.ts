a73841ba98fe13484b2e68758e2627dd53b9ed4b
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-27 09:08:28 +0400

    remove old ui-logo package

--- ui/kit/lib/kit.server.ts
+++ ui/kit/lib/kit.server.ts
@@ -2,5 +2,3 @@ export * from "@onlyoffice/documentation-ui-article-card"
 export * from "@onlyoffice/documentation-ui-content"
 export * from "@onlyoffice/documentation-ui-font"
 export * from "@onlyoffice/documentation-ui-sr-only"
-
-export * from "@onlyoffice/documentation-ui-logo"
