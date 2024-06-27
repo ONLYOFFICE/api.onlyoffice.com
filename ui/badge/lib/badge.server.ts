f2cbfebdb736eff35b6ff4a58223ab5ded2a4541
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-27 09:03:17 +0400

    remove old ui-badge package

--- ui/badge/lib/badge.server.ts
+++
@@ -1,7 +0,0 @@
-import type * as T from "./Badge.tsx"
-import * as C from "./Badge.tsx"
-
-export type BadgeParameters = T.RootParameters
-export type BadgeVariant = T.RootVariant
-
-export const Badge = Object.assign(C.Root, {})
