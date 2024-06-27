91315508bef5313bd6698f27d585d37416f729ec
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-27 11:47:56 +0400

    add colors for the console

--- packages/console/lib/main.ts
+++ packages/console/lib/main.ts
@@ -1,4 +1,5 @@
 import {Console as NodeConsole} from "node:console"
+import {red, yellow} from "kleur/colors"
 
 export class Console extends NodeConsole {
   #name: string
@@ -13,15 +14,18 @@ export class Console extends NodeConsole {
   }
 
   log(...data: Parameters<typeof NodeConsole["prototype"]["log"]>): void {
-    super.log(`${this.#prefix()} info:`, ...data)
+    const p = `${this.#prefix()} info:`
+    super.log(p, ...data)
   }
 
   warn(...data: Parameters<typeof NodeConsole["prototype"]["warn"]>): void {
-    super.warn(`${this.#prefix()} warn:`, ...data)
+    const p = yellow(`${this.#prefix()} warn:`)
+    super.warn(p, ...data)
   }
 
   error(...data: Parameters<typeof NodeConsole["prototype"]["error"]>): void {
-    super.error(`${this.#prefix()} error:`, ...data)
+    const p = red(`${this.#prefix()} error:`)
+    super.error(p, ...data)
   }
 
   #prefix(): string {
