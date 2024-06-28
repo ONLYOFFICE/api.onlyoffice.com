71b98de96ab99e92f12f8ad6eee4232bb9615216
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-28 12:38:02 +0400

    add rehype image plugin

--- site/internal/unified.tsx
+++ site/internal/unified.tsx
@@ -1,3 +1,30 @@
-// import {rehypeEleventyImage} from "@onlyoffice/rehype-eleventy-img"
+import {join} from "node:path"
+import {type Root} from "hast"
+import {visit} from "unist-util-visit"
 
-export function rehypeImage() {}
+export function rehypeImage() {
+  return function (t: Root) {
+    visit(t, "element", (n) => {
+      if (n.tagName !== "img") {
+        return
+      }
+
+      let s = n.properties.src
+
+      if (!s || typeof s !== "string") {
+        return
+      }
+
+      if (s && typeof s === "string" && !s.startsWith("/")) {
+        s = join("assets/images/", s)
+      }
+
+      n.properties = {
+        decoding: "async",
+        loading: "lazy",
+        ...n.properties,
+        src: s
+      }
+    })
+  }
+}
