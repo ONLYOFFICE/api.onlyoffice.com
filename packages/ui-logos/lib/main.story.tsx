cdf19c8063a1880918d548e9b0cddc876d8bc21b
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-09-23 15:24:34 +0400

    temp: ui-logos

--- packages/ui-logos/lib/main.story.tsx
+++ packages/ui-logos/lib/main.story.tsx
@@ -1,13 +1,21 @@
 import {type Meta} from "@storybook/preact"
 import {type JSX, h} from "preact"
-import {OnlyofficeLogo} from "./main.tsx"
+import {OnlyofficeBetaLogo, OnlyofficeLogo} from "../dist/main.tsx"
 
-const meta: Meta = {
+export default {
   title: "UI/Logos",
-}
+} satisfies Meta
 
-export function Composition(): JSX.Element {
-  return <OnlyofficeLogo height={38} />
+export function Default(): JSX.Element {
+  return <div
+    style={{
+      alignItems: "start",
+      display: "flex",
+      flexDirection: "column",
+      gap: 20,
+    }}
+  >
+    <OnlyofficeLogo height={38} />
+    <OnlyofficeBetaLogo height={38} />
+  </div>
 }
-
-export default meta
