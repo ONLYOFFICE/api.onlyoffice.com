aa0b2abfb5dbf21efa95a636c7ebbb460d77e9b9
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-07 12:45:47 +0400

    temp: update ui-pictures package

--- packages/ui-pictures/lib/main.tsx
+++ packages/ui-pictures/lib/main.tsx
@@ -1,32 +1,28 @@
+import * as Elements from "@onlyoffice/preact-elements"
 import {SpikyWow} from "@onlyoffice/ui-mascots"
+import {clsx} from "clsx"
 import {type JSX, h} from "preact"
 import {Picture404Overflow, Picture404Underflow} from "../dist/main.tsx"
 
-export interface Picture404Properties {
+export interface Picture404Properties extends Omit<Elements.DivProperties, "size"> {
   label?: string
   size?: "default" | "small" | "medium" | "large"
 }
 
 export function Picture404(p: Picture404Properties): JSX.Element {
-  const a: JSX.HTMLAttributes<HTMLDivElement> = {
-    class: "picture-404",
-  }
+  const {label, size, ...o} = p
 
-  if (p.label) {
-    a["aria-label"] = p.label
+  if (label) {
+    o["aria-label"] = label
   } else {
-    a["aria-label"] = "Picture of a 404 error"
+    o["aria-label"] = "Picture of a 404 error"
   }
 
-  // The error mentioned below is a false positive.
-  // eslint-disable-next-line unicorn/explicit-length-check
-  if (typeof a.class === "string" && p.size) {
-    a.class += ` picture-404_size_${p.size}`
-  }
+  o.class = clsx("picture-404", size && `picture-404_size_${size}`, o.class)
 
-  return <div {...a}>
+  return <Elements.Div {...o}>
     <Picture404Underflow width={587} />
     <SpikyWow width={165} />
     <Picture404Overflow width={587} />
-  </div>
+  </Elements.Div>
 }
