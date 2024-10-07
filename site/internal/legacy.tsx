b3c60b7cf4bc00a39f3db5cc463aa16f9050acf7
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-07 11:55:53 +0400

    temp: update ui-button package site deps

--- site/internal/legacy.tsx
+++ site/internal/legacy.tsx
@@ -1,10 +1,10 @@
-import {LinkButton} from "@onlyoffice/ui-kit"
+import {Button} from "@onlyoffice/ui-kit"
 import {type JSX, h} from "preact"
 
 export function Legacy(): JSX.Element {
   return <legacy-container class="legacy">
-    <LinkButton variant="accent">
-      Old version
-    </LinkButton>
+    <Button asChild variant="accent">
+      <a>Old version</a>
+    </Button>
   </legacy-container>
 }
