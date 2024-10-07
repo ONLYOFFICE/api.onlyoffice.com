3ac080c85c4827fe7ec3754fe7faffe8660b91ce
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-07 11:16:37 +0400

    temp: add preact-elements package

---
+++ packages/preact-elements/lib/main.tsx
@@ -0,0 +1,36 @@
+// This file is a simplified version of the [@radix-ui/react-primitive] package.
+//
+// [@radix-ui/react-primitives]: https://github.com/radix-ui/primitives/blob/74b182b401c8ca0fa5b66a5a9a47f507bb3d5adc/packages/react/primitive/src/Primitive.tsx
+
+import {Slot} from "@onlyoffice/preact-slots"
+import {type JSX, h} from "preact"
+
+export const A = wrap("a")
+export const Button = wrap("button")
+export const Div = wrap("div")
+
+export interface Element<E extends keyof JSX.IntrinsicElements> {
+  (properties: ElementProperties<E>): JSX.Element
+}
+
+export type ElementProperties<E extends keyof JSX.IntrinsicElements>
+  = JSX.IntrinsicElements[E] & ElementAdditionalProperties
+
+export interface ElementAdditionalProperties {
+  asChild?: boolean
+}
+
+function wrap<E extends keyof JSX.IntrinsicElements>(e: E): Element<E> {
+  return function Element(p: ElementProperties<E>): JSX.Element {
+    const {asChild, ...o} = p
+
+    // It is difficult to type this properly.
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let E: any = e
+    if (asChild) {
+      E = Slot
+    }
+
+    return <E {...o} />
+  }
+}
