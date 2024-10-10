4f9d180b7f4acb758265c2e6780b9ecce436c137
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-10-10 15:29:30 +0300

    update jsdoc fixtures

--- packages/jsdoc-resource-fixtures/fixtures/resource.js
+++ packages/jsdoc-resource-fixtures/fixtures/resource.js
@@ -1,22 +1,66 @@
-/**
- * @class
- */
-class Resource {
+class Cart {
   /**
-   * @param {string} a
-   * @param {number} b
+   * @param {string} userId
+   * @param {Array.<Object>} items
    */
-  constructor(a, b) {}
+  constructor(userId, items) {}
 
   /**
-   * @param {string} a
+   * @param {string} itemId
+   * @param {number} quantity
    * @returns {boolean}
    */
-  method(a) {}
+  addItem(itemId, quantity) {}
+
+  /**
+   * @param {Array.<number|string>} productIds
+   * @param {Object[]} itemDetails
+   * @param {Array.<number[]|string[]>} groupedItems
+   * @param {Array.<string>} itemNames
+   * @param {Array.<number[]|number>} itemQuantities
+   * @param {number[] | number | string[]} mixedValues
+   * @param {boolean} isActive
+   * @param {number} retryCount
+   * @returns {boolean}
+   */
+  processCart(productIds, itemDetails, groupedItems, itemNames, itemQuantities, mixedValues, isActive, retryCount) {}
+}
+
+class Account {
+  /**
+   * @param {string} username
+   * @param {number} balance
+   * @param {Array.<string>} transactionHistory
+   */
+  constructor(username, balance, transactionHistory) {}
+
+  /**
+   * @param {string} accountId
+   * @param {number} depositAmount
+   * @returns {boolean}
+   */
+  deposit(accountId, depositAmount) {}
+
+  /**
+   * @param {string} accountId
+   * @param {Array.<number|string>} transactions
+   * @param {Object[]} transactionDetails
+   * @param {Array.<number[]|string[]>} categorizedTransactions
+   * @returns {boolean}
+   */
+  auditTransactions(accountId, transactions, transactionDetails, categorizedTransactions) {}
 }
 
 /**
- * @param {string} a
- * @param {number} b
+ * @param {string} productId
+ * @param {number} quantity
  */
-function foo(a, b) {}
+function updateStock(productId, quantity) {}
+
+/**
+ * @param {Array.<Object>} data
+ * @param {Array.<number|string>} identifiers
+ * @param {number} maxLimit
+ * @returns {boolean}
+ */
+function validateData(data, identifiers, maxLimit) {}
