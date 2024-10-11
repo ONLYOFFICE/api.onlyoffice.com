16f0f69983d510d4eaa639e431e74e4c49c611e3
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-11 15:56:18 +0400

    actualize the badge component to match the new design

--- packages/ui-badge/lib/main.story.tsx
+++ packages/ui-badge/lib/main.story.tsx
@@ -1,15 +1,61 @@
-import {type Meta} from "@storybook/preact"
-import {Fragment, type JSX, h} from "preact"
-import {Badge} from "./main.tsx"
+import {type Meta, type StoryObj} from "@storybook/preact"
+import {h} from "preact"
+import {Badge, BadgeCaption, type BadgeProperties} from "./main.tsx"
 
 export default {
-  title: "UI/Badge",
+  title: "UI / Badge",
 } satisfies Meta
 
-export function Default(): JSX.Element {
-  return <>
-    <Badge>default</Badge>
-    <Badge variant="danger">danger</Badge>
-    <Badge variant="support">support</Badge>
-  </>
+export const Default: StoryObj = {
+  parameters: {
+    controls: {
+      disable: true,
+    },
+    actions: {
+      disable: true,
+    },
+  },
+  render() {
+    return <Badge>Default</Badge>
+  },
+}
+
+interface PlaygroundProperties extends BadgeProperties {
+  caption: string
+  text: string
+}
+
+export const Playground: StoryObj<PlaygroundProperties> = {
+  parameters: {
+    actions: {
+      disable: true,
+    },
+  },
+  argTypes: {
+    variant: {
+      control: "select",
+      options: [
+        "default",
+        "calm",
+        "critical",
+        // "innovative",
+        "neutral",
+        "transparent",
+        // "vibrant",
+      ],
+    },
+  },
+  args: {
+    variant: "default",
+    caption: "Story",
+    text: "Playground",
+  },
+  render(p) {
+    return <Badge variant={p.variant}>
+      <BadgeCaption>
+        {p.caption}
+      </BadgeCaption>
+      {p.text}
+    </Badge>
+  },
 }
--- packages/ui-badge/lib/main.tsx
+++ packages/ui-badge/lib/main.tsx
@@ -1,25 +1,46 @@
-import {type ChildrenIncludable} from "@onlyoffice/preact-types"
-import {type JSX, h} from "preact"
+import * as Elements from "@onlyoffice/preact-elements"
+import {clsx} from "clsx"
+import {
+  type ComponentChild,
+  Fragment,
+  type JSX,
+  cloneElement,
+  h,
+  toChildArray,
+} from "preact"
 
-export interface BadgeProperties extends ChildrenIncludable {
-  variant?: "danger" | "default" | "support"
+export interface BadgeProperties extends Elements.SpanProperties {
+  variant?:
+    "default" |
+    "calm" |
+    "critical" |
+    "innovative" |
+    "neutral" |
+    "transparent" |
+    "vibrant"
 }
 
 export function Badge(p: BadgeProperties): JSX.Element {
-  return <span class={cls()}>{p.children}</span>
+  const {children, variant, ...o} = p
+  const c: ComponentChild[] = []
 
-  function cls(): string {
-    let s = "badge"
-    switch (p.variant) {
-    case "danger":
-      s += " badge_danger"
-      break
-    case "default":
-      break
-    case "support":
-      s += " badge_support"
-      break
+  o.class = clsx("badge", variant && `badge_variant_${variant}`, o.class)
+
+  const a = toChildArray(children)
+  for (let e of a) {
+    if (typeof e === "object" && e.type === BadgeCaption) {
+      e = cloneElement(e, {children: <>{e.props.children}{":"}</>})
+      c.unshift(<>{e}{" "}</>)
+      continue
     }
-    return s
+    c.push(e)
   }
+
+  return <Elements.Span {...o}>{c}</Elements.Span>
+}
+
+export function BadgeCaption(p: Elements.SpanProperties): JSX.Element {
+  const {...o} = p
+  o.class = clsx("badge__caption", o.class)
+  return <Elements.Span {...o} />
 }
