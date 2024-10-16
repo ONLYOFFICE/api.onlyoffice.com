e9c2a1c4889c3c66ac84269b7e6ab1b7f7e98086
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-16 15:38:57 +0400

    temp: up internal site-service

--- site/layouts/service-declaration.tsx
+++ site/layouts/service-declaration.tsx
@@ -1,22 +1,13 @@
 import {type Context, type Data} from "@onlyoffice/eleventy-types"
-import {Fragment, type JSX, h} from "preact"
-import {ArticleContent, ArticleSidebar} from "@/internal/chapter.tsx"
-import {Service, ServiceToc} from "@/internal/service.tsx"
+import {type JSX, h} from "preact"
+import {Service} from "@/internal/service.tsx"
 
 export function data(): Data {
   return {
     layout: "chapter",
-    tempChapterNext: true,
   }
 }
 
 export function render(c: Context): JSX.Element {
-  return <>
-    <ArticleSidebar>
-      {() => <ServiceToc />}
-    </ArticleSidebar>
-    <ArticleContent>
-      {() => <Service url={c.page.url} />}
-    </ArticleContent>
-  </>
+  return <Service url={c.page.url} />
 }
