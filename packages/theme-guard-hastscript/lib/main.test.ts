785107be10276d21440ca68d2d11c58dd3ad9956
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-11-13 13:27:01 +0400

    temp: add theme-guard-hastscript package

---
+++ packages/theme-guard-hastscript/lib/main.test.ts
@@ -0,0 +1,4 @@
+// import {type Component, h, render} from "@onlyoffice/hastscript"
+// import {test} from "uvu"
+// import {equal as eq} from "uvu/assert"
+// import {ThemeGuard} from "./main.ts"
---
+++ packages/theme-guard-hastscript/lib/main.ts
@@ -0,0 +1,5 @@
+// import {type Children, type Component, h} from "@onlyoffice/hastscript"
+// import type * as E from "@onlyoffice/hastscript/elements.ts"
+// import {clsx} from "clsx"
+
+// export function ThemeGuard()
