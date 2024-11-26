0b62e15d9f6c8ef55726f2cafc77ed553fe5e22a
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-11-26 15:57:23 +0400

    temp

--- site/resources/docspace-plugin-sdk.ts
+++ site/resources/docspace-plugin-sdk.ts
@@ -2,7 +2,7 @@ import {Resource as S} from "@onlyoffice/docspace-plugin-sdk-resource"
 import {isBuild} from "@onlyoffice/eleventy-env"
 import {Resource as F} from "@onlyoffice/fixture-resource/typedoc.ts"
 
-export const Resource = r()
+export const Resource = S // r()
 
 function r(): typeof S | typeof F {
   if (isBuild()) {
