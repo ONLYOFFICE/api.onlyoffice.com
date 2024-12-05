c19c3b2bcc46da0a8e65356df9cf8a6d9ad24c9d
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-12-05 11:20:53 +0400

    temp: add lang-to-ext to mdast-util-eslint

--- packages/mdast-util-eslint/lib/main.ts
+++ packages/mdast-util-eslint/lib/main.ts
@@ -1,4 +1,5 @@
 import {ESLint} from "@onlyoffice/eslint-config"
+import {langToExt} from "@onlyoffice/lang-to-ext"
 import {type Code, type Nodes} from "mdast"
 import {visit} from "unist-util-visit"
 
@@ -20,7 +21,12 @@ async function lint(n: Code): Promise<void> {
     return
   }
 
-  const a = await e.lintText(n.value, {filePath: `.md/.${n.lang}`})
+  const x = langToExt(n.lang)
+  if (!x) {
+    return
+  }
+
+  const a = await e.lintText(n.value, {filePath: `.md/${x}`})
   if (a.length === 0) {
     return
   }
