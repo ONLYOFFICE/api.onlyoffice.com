0a1c8e8a0e8fda40bfe5e7016605c125df36e848
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-12-13 16:00:38 +0300

    import typedoc-signature to typedoc-declaration

--- packages/typedoc-declaration/lib/main.ts
+++ packages/typedoc-declaration/lib/main.ts
@@ -1,6 +1,7 @@
 import {pipeline} from "node:stream"
 import {promisify} from "node:util"
 import type * as L from "@onlyoffice/library-declaration/next.ts"
+import {compute} from "@onlyoffice/typedoc-signature"
 import {Transformer} from "@onlyoffice/typedoc-transformer"
 import {Transport} from "@onlyoffice/typedoc-transport"
 import {type JSONOutput as J} from "typedoc"
@@ -19,5 +20,6 @@ export async function process(o: J.Reflection): Promise<L.Entity[]> {
 
   console.log(`Finish processing ${o.name} #${o.id}`)
 
+  compute(tp)
   return tp.entities
 }
