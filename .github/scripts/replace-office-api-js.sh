a55611f10e39db0ad37c9cb937453c0a3db047da
danilapog <danil.titarenko@onlyoffice.com>
2025-03-04 18:39:07 +0300

    Refactoring code

--- .github/scripts/replace-office-api-js.sh
+++ .github/scripts/replace-office-api-js.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-cd ${GITHUB_WORKSPACE}
+#set -eux
+
+MAIN_DIR="${GITHUB_WORKSPACE}/replace-js"
 
 # github.event_name CAN BE ACCESSED via bash variable: ${EVENT}
 # github.ref_name CAN BE ACCESSED via bash variable: ${REF}
 # github user token CAN BE ACCESSED via bash variable: ${TOKEN_PUBLICK_REPO}
 # private repo token CAN BE ACCESSED via bash variabel: ${TOKEN_PRIVATE_REPO}
-# private repo username CAN BE ACCESSED via bash variable: ${USER_PRIVATE_REPO}
+# private repo username CAN BE ACCESSED via bash variable: ${USER_PRIVATE_NAME}
 # repo url CAN BE ACCESSED via bash variable: ${URL_PRIVATE_REPO}
+# private user mail CAN BE ACCESSED via bash variable: ${USER_PRIVATE_MAIL}
 
 
 repos=(
@@ -48,45 +51,67 @@ function make_branches() {
     echo "Check branches in repo: ${COLOR_GREEN}${repo}${COLOR_RESET}"
     for branch in ${BRANCHES[@]}; do
         echo "Check branch: ${COLOR_GREEN}${branch}${COLOR_RESET}"
-        branch=$(curl -s -L \
+        exist_branch=$(curl -s -L \
               -H "Accept: application/vnd.github+json" \
               -H "Authorization: Bearer ${TOKEN_PUBLICK_REPO}" \
               -H "X-GitHub-Api-Version: 2022-11-28" \
               https://api.github.com/repos/ONLYOFFICE/${repo}/branches | \
               jq -r --arg branch "$branch" '.[] | select(.name | startswith($branch)) | .name')
-         if [[ -z "${branch}" ]]; then
+         if [[ -z "${exist_branch}" ]]; then
              echo "${COLOR_YELLOW}Branch ${branch} is not exist in repo ${repo}. Make it...${COLOR_RESET}"
-            [[ ! -d ${repo} ]] && git clone https://${USER_PRIVATE_REPO}:${TOKEN_PRIVATE_REPO}@${URL_PRIVATE_REPO}/ONLYOFFICE/${repo}.git
+            [[ ! -d ${repo} ]] && git clone https://${USER_PRIVATE_NAME}:${TOKEN_PRIVATE_REPO}@${URL_PRIVATE_REPO}/ONLYOFFICE/${repo}.git
             cd ${repo}
-            git checkout master
-            git checkout -b ${branch} && git push origin ${branch} && cd ${GITHUB_WORKSPACE}
+            git branch "${branch}" && git push origin ${branch} && cd ${MAIN_DIR}
             echo "${COLOR_GREEN}Branch ${branch} was created in repo ${repo}...${COLOR_RESET}"
+         else 
+            echo "${COLOR_GREEN}Branch ${branch} is exist in repo ${repo}... Skip...${COLOR_RESET}"
          fi
      done
      rm -rf ${repo}
   done
   # Move back to working dir
-  cd ${GITHUB_WORKSPACE}
+  cd ${MAIN_DIR}
 }
 
 function push_changes() {
-  for branch in ${branches[@]}; do
-    cd ${GITHUB_WORKSPACE}
-    mkdir ${branch} && cd ${branch}
-    for repo in ${repos[@]}; do
-       git clone --single-branch --branch ${branch} https://${USER_PRIVATE_REPO}:${TOKEN_PRIVATE_REPO}@${URL_PRIVATE_REPO}/ONLYOFFICE/${repo}.git
-    done
+  echo "${COLOR_BLUE}Set git user${COLOR_RESET}"
+  git config --global user.email "${USER_PRIVATE_MAIL}"
+  git config --global user.name "${USER_PRIVATE_NAME}"
+  
+  echo "${COLOR_BLUE}Check changes in office-js-api and make PR with this changes...${COLOR_RESET}"
+  for branch in ${BRANCHES[@]}; do
+    echo "${COLOR_BLUE}Get changes from branch: ${branch}${COLOR_RESET}"
+    cd ${MAIN_DIR}
+    mkdir ${branch##*/} && cd ${branch##*/}
     working_dir=$(pwd)
-    
-    cd ./api.onlyoffice.com && git branch feature/office-js-bot-${branch} && git checkout feature/replace-office-js-bot-${branch} && cd ${working_dir}
+    for repo in ${repos[@]}; do
+       git clone https://${USER_PRIVATE_NAME}:${TOKEN_PRIVATE_REPO}@${URL_PRIVATE_REPO}/ONLYOFFICE/${repo}.git
+       cd ./${repo}
+       git checkout ${branch} 
+       cd ${working_dir}
+    done
+
+    cd ./api.onlyoffice.com
+    if [[ $( git ls-remote origin feature/replace-office-js-bot-${branch//\//-}) ]]; then
+        echo "${COLOR_BLUE}Branch feature/replace-office-js-bot-${branch//\//-} exist in remote repo${COLOR_RESET}"
+        head_branch_exist=true
+    else
+        echo "${COLOR_YELLOW}Branch feature/replace-office-js-bot-${branch//\//-} not exist in api.onlyoffice.com repo, create...${COLOR_RESET}"
+        git branch feature/replace-office-js-bot-${branch//\//-}
+        head_branch_exist=false
+    fi
+    git checkout feature/replace-office-js-bot-${branch//\//-} 
+    cd ${working_dir}
     
     # Replace main api
+    echo "${COLOR_BLUE}Replace main api files...${COLOR_RESET}"
     cp -r ./office-js-api/Word/*  ./api.onlyoffice.com/site/docs/office-api/usage-api/text-document-api/
     cp -r ./office-js-api/Slide/* ./api.onlyoffice.com/site/docs/office-api/usage-api/presentation-api/
     cp -r ./office-js-api/Cell/*  ./api.onlyoffice.com/site/docs/office-api/usage-api/spreadsheet-api/
     cp -r ./office-js-api/Forms/* ./api.onlyoffice.com/site/docs/office-api/usage-api/form-api/
 
     # Replace plugins api
+    echo "${COLOR_BLUE}Replace plugins api files...${COLOR_RESET}"
     cp -r ./office-js-api/Plugins/Cell/*  ./api.onlyoffice.com/site/docs/plugin-and-macros/interacting-with-editors/methods/spreadsheet-api/
     cp -r ./office-js-api/Plugins/Forms/* ./api.onlyoffice.com/site/docs/plugin-and-macros/interacting-with-editors/methods/form-api/
     cp -r ./office-js-api/Plugins/Slide/* ./api.onlyoffice.com/site/docs/plugin-and-macros/interacting-with-editors/methods/presentation-api/
@@ -94,15 +119,33 @@ function push_changes() {
     
     CHANGES=$(cd ./api.onlyoffice.com && git diff-index HEAD --)
     if [[ ! -z "${CHANGES}" ]]; then
-      # COMMIT CHANGES AND MAKE PULL REQUEST
+      echo "${COLOR_BLUE}Changes exist, push them to api.onlyoffice.com${COLOR_RESET}"
       cd ./api.onlyoffice.com
+      
+      ### SHOULD BE REMOVE
+      echo "${COLOR_BLUE}Add changes via git add .${COLOR_RESET}"
       git add .
+
+      ### SHOULD BE REMOVE
+      echo "${COLOR_BLUE}Commit with git commit -m${COLOR_RESET}"     
       git commit -m "Add changes from office-js-api"
-      git push origin feature/office-js-bot-${branch}
+
+      ### SHOULD BE REMOVE
+      echo "${COLOR_BLUE}Push changes with git push origin${COLOR_RESET}" 
+      if [[ "${head_branch_exist}" == 'true' ]]; then
+        echo "${COLOR_BLUE}Push when branch exist${COLOR_RESET}"
+        git push 
+      elif [[ "${head_branch_exist}" == 'false' ]]; then
+        echo "${COLOR_RED}Push when branch non-exist${COLOR_RESET}"
+        git push --set-upstream origin feature/replace-office-js-bot-${branch//\//-}
+      fi
+
+      ### SHOULD BE REMOVE
+      echo "${COLOR_BLUE}Push changes with curl POST ${COLOR_RESET}"
       curl -X POST "https://${URL_PRIVATE_REPO}/api/v1/repos/ONLYOFFICE/api.onlyoffice.com/pulls"  \
            -H "Content-Type: application/json"  \
            -H "Authorization: token ${TOKEN_PRIVATE_REPO}"  \
-           -d "{ \"head\": \"feature/replace-office-js-bot-${branch}\",
+           -d "{ \"head\": \"feature/replace-office-js-bot-${branch//\//-}\",
                  \"base\": \"${branch}\",
                  \"title\": \"Merge changes from office-js-api ${branch}\",
                  \"body\": \"Please check it @trofim24\"
