2e0d1925df289dc3eda0fcbba4d0f6acb22007df
nikita_bartoshuk <nikita.bartoshuk@onlyoffice.com>
2025-09-26 15:24:58 +0300

    upgraded to docusaurus3.8.1 and ejected string

--- src/theme/CodeBlock/Content/String.tsx
+++ src/theme/CodeBlock/Content/String.tsx
@@ -1,139 +1,35 @@
 import React, {type ReactNode} from 'react';
-import clsx from 'clsx';
-import {useThemeConfig, usePrismTheme} from '@docusaurus/theme-common';
+import {useThemeConfig} from '@docusaurus/theme-common';
 import {
-  parseCodeBlockTitle,
-  parseLanguage,
-  parseLines,
-  containsLineNumbers,
+  CodeBlockContextProvider,
+  type CodeBlockMetadata,
+  createCodeBlockMetadata,
   useCodeWordWrap,
 } from '@docusaurus/theme-common/internal';
-import {Highlight, type Language} from 'prism-react-renderer';
-import Line from '@theme/CodeBlock/Line';
-import CopyButton from '@theme/CodeBlock/CopyButton';
-import WordWrapButton from '@theme/CodeBlock/WordWrapButton';
-import Container from '@theme/CodeBlock/Container';
 import type {Props} from '@theme/CodeBlock/Content/String';
+import CodeBlockLayout from '@theme/CodeBlock/Layout';
 
-import styles from './styles.module.css';
-
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
-import OnlyOfficeEditor from '@site/src/components/BrowserWindow/OnlyofficeEditor';
-
-// Prism languages are always lowercase
-// We want to fail-safe and allow both "php" and "PHP"
-// See https://github.com/facebook/docusaurus/issues/9012
-function normalizeLanguage(language: string | undefined): string | undefined {
-  return language?.toLowerCase();
+function useCodeBlockMetadata(props: Props): CodeBlockMetadata {
+  const {prism} = useThemeConfig();
+  return createCodeBlockMetadata({
+    code: props.children,
+    className: props.className,
+    metastring: props.metastring,
+    magicComments: prism.magicComments,
+    defaultLanguage: prism.defaultLanguage,
+    language: props.language,
+    title: props.title,
+    showLineNumbers: props.showLineNumbers,
+  });
 }
 
-export default function CodeBlockString({
-  children,
-  className: blockClassName = '',
-  metastring,
-  title: titleProp,
-  showLineNumbers: showLineNumbersProp,
-  language: languageProp,
-}: Props): ReactNode {
-  const {
-    prism: {defaultLanguage, magicComments},
-  } = useThemeConfig();
-  const language = normalizeLanguage(
-    languageProp ?? parseLanguage(blockClassName) ?? defaultLanguage,
-  );
-
-  const prismTheme = usePrismTheme();
+// TODO Docusaurus v4: move this component at the root?
+export default function CodeBlockString(props: Props): ReactNode {
+  const metadata = useCodeBlockMetadata(props);
   const wordWrap = useCodeWordWrap();
-
-  // We still parse the metastring in case we want to support more syntax in the
-  // future. Note that MDX doesn't strip quotes when parsing metastring:
-  // "title=\"xyz\"" => title: "\"xyz\""
-  const title = parseCodeBlockTitle(metastring) || titleProp;
-
-  const {lineClassNames, code} = parseLines(children, {
-    metastring,
-    language,
-    magicComments,
-  });
-  const showLineNumbers =
-    showLineNumbersProp ?? containsLineNumbers(metastring);
-
-  const editorWord = metastring && metastring.includes("editor-docx") && "docx";
-  const editorCell = metastring && metastring.includes("editor-xlsx") && "xlsx";
-  const editorSlide = metastring && metastring.includes("editor-pptx") && "pptx";
-  const editorPdf = metastring && metastring.includes("editor-pdf") && "pdf";
-  const isForm = metastring && metastring.includes("editor-pdf"); // TODO: change to editor-pdf-form
-  const editorType = editorWord || editorCell || editorSlide || editorPdf;
-
-  let res = metastring ? metastring.match(/zoom=(\d+)\s*/) : null;
-  const zoom = res ? Number(res[1]) : undefined;
-
-  res = metastring ? metastring.match(/templateUrl=([^\s]+)/) : null;
-  const templateUrl = res ? res[1] : undefined;
-
-  const codeBlockContent = (
-    <Container
-      as="div"
-      className={clsx(
-        blockClassName,
-        language &&
-          !blockClassName.includes(`language-${language}`) &&
-          `language-${language}`,
-      )}>
-      {title && <div className={styles.codeBlockTitle}>{title}</div>}
-      <div className={styles.codeBlockContent}>
-        <Highlight
-          theme={prismTheme}
-          code={code}
-          language={(language ?? 'text') as Language}>
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre
-              tabIndex={0}
-              ref={wordWrap.codeBlockRef}
-              className={clsx(className, styles.codeBlock, 'thin-scrollbar')}
-              style={style}>
-              <code
-                className={clsx(
-                  styles.codeBlockLines,
-                  showLineNumbers && styles.codeBlockLinesWithNumbering,
-                )}>
-                {tokens.map((line, i) => (
-                  <Line
-                    key={i}
-                    line={line}
-                    getLineProps={getLineProps}
-                    getTokenProps={getTokenProps}
-                    classNames={lineClassNames[i]}
-                    showLineNumbers={showLineNumbers}
-                  />
-                ))}
-              </code>
-            </pre>
-          )}
-        </Highlight>
-        <div className={styles.buttonGroup}>
-          {(wordWrap.isEnabled || wordWrap.isCodeScrollable) && (
-            <WordWrapButton
-              className={styles.codeButton}
-              onClick={() => wordWrap.toggle()}
-              isEnabled={wordWrap.isEnabled}
-            />
-          )}
-          <CopyButton className={styles.codeButton} code={code} />
-        </div>
-      </div>
-    </Container>
-  );
-
-  return editorType ? (
-    <Tabs lazy>
-      <TabItem value="code" label="Code">{codeBlockContent}</TabItem>
-      <TabItem value="result" label="Result">
-        <OnlyOfficeEditor code={code} fileType={editorType} templateUrl={templateUrl} zoom={zoom} isForm={isForm} />
-      </TabItem>
-    </Tabs>
-  ) : (
-    codeBlockContent
+  return (
+    <CodeBlockContextProvider metadata={metadata} wordWrap={wordWrap}>
+      <CodeBlockLayout />
+    </CodeBlockContextProvider>
   );
 }
