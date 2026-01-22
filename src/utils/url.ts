114666c495a34138fbf857995f574877eb5a7bde
konstantin.kireyev <konstantin.kireyev@onlyoffice.com>
2026-01-22 17:50:05 +0500

    fix: getFullUrl handles SSR fallback

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,8 @@
 export function getFullUrl(localUrl: string): string {
+    if (typeof window === 'undefined') {
+        return localUrl; // SSR fallback
+    }
+
     let url = location.href;
     url = url.substring(0, url.lastIndexOf("/"));
     return url + localUrl;
