fa4319a3c48818a2795604632a2d5adef3604be5
Alexander Trofimov <alexander.trofimov@onlyoffice.com>
2026-01-29 23:03:51 +0300

    add askAI button and panel

---
+++ src/theme/Root.js
@@ -0,0 +1,32 @@
+import { DocSearch } from '@docsearch/core';
+import { Sidepanel, SidepanelButton } from '@docsearch/sidepanel';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from 'react';
+
+import '@docsearch/css/dist/sidepanel.css';
+
+export default function Root({ children }) {
+  const { siteConfig } = useDocusaurusContext();
+  const algoliaConfig = siteConfig.themeConfig.algolia;
+  
+  return (
+    <>
+      {children}
+
+      <BrowserOnly>
+        {() => (
+          <DocSearch>
+            <SidepanelButton />
+            <Sidepanel
+              appId={algoliaConfig.appId}
+              apiKey={algoliaConfig.apiKey}
+              indexName={algoliaConfig.indexName}
+              assistantId={algoliaConfig.askAi}
+            />
+          </DocSearch>
+        )}
+      </BrowserOnly>
+    </>
+  );
+}
