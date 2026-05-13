a6e0bb584e73bb7c52993687dbbac454d71541fb
Alexander Trofimov <alexander.trofimov@onlyoffice.com>
2026-05-13 10:58:43 +0300

    fix(config): support italic markdown in config editor tooltips

--- src/components/ConfigEditor/renderers/utils/Tooltip.tsx
+++ src/components/ConfigEditor/renderers/utils/Tooltip.tsx
@@ -7,6 +7,7 @@ function formatDescription(text: string): string {
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
         .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+        .replace(/\*(.+?)\*/g, '<em>$1</em>')
         .replace(/`(.+?)`/g, '<code>$1</code>');
 }
 
